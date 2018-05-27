ed6d5bc01c09e68a0cb73b439c6bc555615f7577
devkoo <ysg9350@gmail.com>
2018-05-27 22:04:12 +0900

    Chore: Noticeboard; list
    dao play roles accesses to DB and converts data into java source in
    order to do the job with Java

--- src/main/java/com/javalec/spring_pjt_board_dao/BDao.java
+++ src/main/java/com/javalec/spring_pjt_board_dao/BDao.java
@@ -1,5 +1,16 @@
 package com.javalec.spring_pjt_board_dao;
 
+import java.util.ArrayList;
+
+import com.javalec.spring_pjt_board_dto.BDto;
+
 public class BDao {
+
+	public ArrayList<BDto> list() {
+		
+		ArrayList<BDto> dtos = null;
+		
+		return dtos;
+	}
 
 }
